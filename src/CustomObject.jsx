e223f7595fabd5c4a286fa3c113cd098f39fce63
fibini <fabienswift@gmail.com>
2023-01-25 14:58:16 -0400

    create a custom geometry

--- src/CustomObject.jsx
+++ src/CustomObject.jsx
@@ -1,4 +1,23 @@
+import * as Three from 'three'
+
 export default function CustomObject()
 {
-    return null
+    const verticesCount = 10 * 3
+    const positions = new Float32Array(verticesCount * 3)
+
+    for(let i = 0; i < verticesCount * 3; i++)
+    {
+        positions[i] = (Math.random() -0.5) * 3
+    }
+    return <mesh>
+        <bufferGeometry>
+            <bufferAttribute 
+                attach="attributes-position"
+                count={ verticesCount }
+                itemSize={ 3 }
+                array={ positions }
+            />
+        </bufferGeometry>
+        <meshBasicMaterial color= 'red' side={ Three.DoubleSide } />
+    </mesh>
 }
