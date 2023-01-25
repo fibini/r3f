dd9a4c8dd13027faa7d0c3cf96b0d4160d0c534e
fibini <fabienswift@gmail.com>
2023-01-25 13:19:14 -0400

    create a component with sphere geometry

---
+++ src/Experience.jsx
@@ -0,0 +1,9 @@
+export default function Experience()
+{
+    return <>
+        <mesh>
+            <sphereGeometry  args={[1.5, 32, 32]}/>
+            <meshBasicMaterial />
+        </mesh>
+    </>
+}
