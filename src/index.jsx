b4a471014ca593ec96ba85fe084b2ebba9ca150a
fibini <fabienswift@gmail.com>
2023-01-25 12:09:12 -0400

    commit msg hi

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <>
+        <Canvas></Canvas>
+    </>
+)
